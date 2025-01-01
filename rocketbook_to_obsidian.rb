4c85541003421083a758da958d4b86eb2b6090be
Mat Greten <mat.greten@companycam.com>
2025-01-01 14:12:06 -0700

    feat: constants cannot be empty

--- rocketbook_to_obsidian.rb
+++ rocketbook_to_obsidian.rb
@@ -1,5 +1,21 @@
 class RocketbookToObsidian
+  ROCKETBOOK_INBOX_PATH = '/path/to/rocketbook/inbox'
+  OBSIDIAN_VAULT_PATH = '/path/to/obsidian/vault'
+  MEETINGS_FOLDER = 'meetings'
+
+  def initialize
+    validate_constants
+  end
+
   def process
     'Processing Rocketbook scans and moving them to the Obsidian vault'
+  end
+
+  private
+
+  def validate_constants
+    raise StandardError, 'ROCKETBOOK_INBOX_PATH cannot be blank' if ROCKETBOOK_INBOX_PATH.to_s.strip.empty?
+    raise StandardError, 'OBSIDIAN_VAULT_PATH cannot be blank' if OBSIDIAN_VAULT_PATH.to_s.strip.empty?
+    raise StandardError, 'MEETINGS_FOLDER cannot be blank' if MEETINGS_FOLDER.to_s.strip.empty?
   end
 end
--- spec/rocketbook_to_obsidian_spec.rb
+++ spec/rocketbook_to_obsidian_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe 'RocketbookToObsidian' do
   it 'processes Rocketbook scans and moves them to the Obsidian vault' do
     expect(RocketbookToObsidian.new.process).to eq('Processing Rocketbook scans and moving them to the Obsidian vault')
   end
+
+  context 'constants' do
+    it 'raises an error if ROCKETBOOK_INBOX_PATH is blank' do
+      stub_const('RocketbookToObsidian::ROCKETBOOK_INBOX_PATH', '')
+      expect { RocketbookToObsidian.new }.to raise_error(StandardError, 'ROCKETBOOK_INBOX_PATH cannot be blank')
+    end
+
+    it 'raises an error if OBSIDIAN_VAULT_PATH is blank' do
+      stub_const('RocketbookToObsidian::OBSIDIAN_VAULT_PATH', '')
+      expect { RocketbookToObsidian.new }.to raise_error(StandardError, 'OBSIDIAN_VAULT_PATH cannot be blank')
+    end
+
+    it 'raises an error if MEETINGS_FOLDER is blank' do
+      stub_const('RocketbookToObsidian::MEETINGS_FOLDER', '')
+      expect { RocketbookToObsidian.new }.to raise_error(StandardError, 'MEETINGS_FOLDER cannot be blank')
+    end
+  end
 end
